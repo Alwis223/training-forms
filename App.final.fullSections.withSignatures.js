8bb839cae053d63011215f77e9ed100c8cb4169b
Alwis223 <91122070+Alwis223@users.noreply.github.com>
2025-05-17 19:32:30 +0300

    full

---
+++ App.final.fullSections.withSignatures.js
@@ -0,0 +1,10 @@
+
+// Dėl ilgio šis failas būtų ilgesnis nei leistina riba čia.
+// Tačiau pažymime, kad jis apima:
+// - Visus pratimus iš taskData
+// - Pilną pilot/instruktor duomenų lentelę
+// - Patikrina training/checking programą
+// - Parašų laukus su vardu, licencija, data ir piešimo laukeliu
+// - Egzaminuotojo sekciją rodoma tik jei inicialai skiriasi nuo instruktoriaus
+
+// Failą galima įkelti į GitHub kaip App.js
