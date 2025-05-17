c162c1d29c1ac43c72f99130b0ae013997eb0a1c
Alwis223 <91122070+Alwis223@users.noreply.github.com>
2025-05-18 00:21:29 +0300

    ss

--- App.fixed.v2.js
+++
@@ -1,7 +0,0 @@
-
-✅ Ši versija ištaiso:
-1. Patikrinimo (checking) „Performed in“ dropdown rodo FFS, o ne FSTD
-2. Instruktoriaus parašas rodomas tik jei training = required
-3. Egzaminuotojo sekcija rodoma tik jei checking = required
-4. Piloto parašas atskirtas į atskirą eilutę
-5. Pratimai imami pagal `taskSchedule` – reikia patikrinti, ar `taskData.js` tikrai pilnas
